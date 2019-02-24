86faac1b877408bfa5aecf6dc816c128bc556ad6
vitoSctt <47935899+vitoSctt@users.noreply.github.com>
2019-02-24 22:27:27 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Preloader
+	$(window).on('load', function () {
+		$preloader = $('.loaderArea'),
+			$loader = $preloader.find('.loader');
+		$loader.fadeOut();
+		$preloader.delay(350).fadeOut('slow');
+	});
+// END Preloader
+
